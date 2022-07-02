f722443d757a195a97ef9ccb12f1f633eb44fb3e
wildflowerlemon <madipup16@gmail.com>
2022-07-01 23:44:50 -0600

    directory

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,4 +1,4 @@
-const requestURL = "data.json"
+const requestURL = "https://wildflowerlemon.github.io/wdd230/chamber/data/data.json"
 const cards = document.querySelector(".cards");
 
 fetch(requestURL)
@@ -18,12 +18,15 @@ function displayCompanies(company) {
     let picture = document.createElement("img");
     let address = document.createElement("p");
     let phonenumber = document.createElement("p");
-    let website = documemnt.createElement("p");
+    let website = document.createElement("p");
+    let membership = document.createElement("p");
+
 
     h2.textContent = `${company.name}`;
     address.innerHTML = `${company.address}`;
-    phonenumber.innerHTML = `${company.phonenumber}`
+    phonenumber.innerHTML = `${company.phonenumber}`;
     website.innerHTML = `${company.website}`;
+    membership.innerHTML = `${company.membership}`;
 
 
     //Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
@@ -33,9 +36,10 @@ function displayCompanies(company) {
     picture.setAttribute("loading", "lazy");
 
     card.appendChild(h2);
-    card.appendChild();
-    card.appendChild(place);
     card.appendChild(picture);
+    card.appendChild(address);
+    card.appendChild(phonenumber);
+    card.appendChild(membership);
 
     // Add/apend the existing HTML div with the cards class with the section(card)
     document.querySelector("div.cards").appendChild(card);
