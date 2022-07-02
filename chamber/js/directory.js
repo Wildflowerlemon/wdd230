3beeb8eac751db3d553b0a0b8ba5f3a75fbd4908
wildflowerlemon <madipup16@gmail.com>
2022-07-02 04:12:03 -0600

    directory

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -47,3 +47,18 @@ function displayCompanies(company) {
     document.querySelector("div.cards").appendChild(card);
 }
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid")
+}
