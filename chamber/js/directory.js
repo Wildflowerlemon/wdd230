d485c50eb190526fd6a648a563944c14484acfb3
wildflowerlemon <madipup16@gmail.com>
2022-07-02 16:09:22 -0600

    responsive

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -11,42 +11,51 @@ fetch(requestURL)
         companies.forEach(displayCompanies);
     });
 
+
+
 function displayCompanies(company) {
-    //Create elements to add to the document
-    let card = document.createElement("section");
-    let h2 = document.createElement("h2");
-    let picture = document.createElement("img");
-    let address = document.createElement("p");
-    let phonenumber = document.createElement("p");
-    let website = document.createElement("p");
-    let membership = document.createElement("p");
+    //const goldsilcompanies = company.filter( membership => membership.membership == "Gold Member" || "Silver Member");
+    //goldsilcompanies.forEach(company => {
+
+        //Create elements to add to the document
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let picture = document.createElement("img");
+        let address = document.createElement("p");
+        let phonenumber = document.createElement("p");
+        let website = document.createElement("p");
+        let membership = document.createElement("p");
 
 
-    h2.textContent = `${company.name}`;
-    picture.innerHTML = `${company.picture}`
-    address.innerHTML = `${company.address}`;
-    phonenumber.innerHTML = `${company.phonenumber}`;
-    website.innerHTML = `${company.website}`;
-    membership.innerHTML = `${company.membership}`;
+        h2.textContent = `${company.name}`;
+        picture.innerHTML = `${company.picture}`
+        address.innerHTML = `${company.address}`;
+        phonenumber.innerHTML = `${company.phonenumber}`;
+        website.innerHTML = `${company.website}`;
+        membership.innerHTML = `${company.membership}`;
 
 
-    //Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    picture.setAttribute("src", company.imageurl);
-    picture.setAttribute("alt",`Picture of ${company.name}`);
-    
-    picture.setAttribute("loading", "lazy");
+        //Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+        picture.setAttribute("src", company.imageurl);
+        picture.setAttribute("alt",`Picture of ${company.name}`);
+        
+        picture.setAttribute("loading", "lazy");
 
-    card.appendChild(h2);
-    card.appendChild(picture);
-    card.appendChild(address);
-    card.appendChild(phonenumber);
-    card.appendChild(website);
-    card.appendChild(membership);
+        card.appendChild(h2);
+        card.appendChild(picture);
+        card.appendChild(address);
+        card.appendChild(phonenumber);
+        card.appendChild(website);
+        card.appendChild(membership);
 
-    // Add/apend the existing HTML div with the cards class with the section(card)
-    document.querySelector("div.cards").appendChild(card);
+        // Add/apend the existing HTML div with the cards class with the section(card)
+        document.querySelector("div.cards").appendChild(card);
 }
 
+
+
+
+//---------------- Directory Grid/List ----------------
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector(".cards");
